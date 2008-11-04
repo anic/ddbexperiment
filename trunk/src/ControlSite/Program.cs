81d702bde03f1123edaf94b218f4c8a732e4af89
yaoancheng <yaoancheng@1f297eaa-a758-11dd-9a14-c1645b63b0c6>
2008-11-04 12:07:33 +0000

    定格： 将DataAccess删除，作为ServerSite的一个子命名空间 将RelationalAlgebra转移到Common中。 在ControlSite中删除了已经完成的用例

--- trunk/src/ControlSite/Program.cs
+++ trunk/src/ControlSite/Program.cs
@@ -33,6 +33,9 @@ namespace DistDBMS.ControlSite
 
         Selection s1, s2; //作为测试使用
 
+        /// <summary>
+        /// 关系代数的填写方法
+        /// </summary>
         private void TestRelationAlgebra()
         {
             //Sample1
@@ -136,147 +139,11 @@ namespace DistDBMS.ControlSite
             
         }
 
+        /// <summary>
+        /// 测试SQL转换
+        /// </summary>
         private void TestSQLSyntax()
         {
-            //Sample1
-            //关系代数树，示范结构：select * from Course where credit_hour>2 and location='CB‐6'
-            s1 = new Selection();
-            s1.Fields.IsAllFields = true;
-            s1.Fields.TableName = "Course";
-
-            TableScheme table = new TableScheme();
-            table.TableName = "Course";
-            table.IsAllFields = true;
-            s1.Sources.Add(table);
-            s1.Condition = new Condition();
-            s1.Condition.Operator = RelationOperator.And;
-
-            //左条件
-            s1.Condition.LeftCondition = new Condition();
-            s1.Condition.LeftCondition.AtomCondition = new AtomCondition();
-            s1.Condition.LeftCondition.AtomCondition.Content = "credit_hour>2";
-            s1.Condition.LeftCondition.AtomCondition.Operator = LogicOperator.Greater;
-            //Course.credit_hour
-            s1.Condition.LeftCondition.AtomCondition.LeftOperand.IsValue = false;
-            s1.Condition.LeftCondition.AtomCondition.LeftOperand.Field.TableName = "Course";
-            s1.Condition.LeftCondition.AtomCondition.LeftOperand.Field.AttributeName = "credit_hour";
-            //2
-            s1.Condition.LeftCondition.AtomCondition.RightOperand.IsValue = true;
-            s1.Condition.LeftCondition.AtomCondition.RightOperand.ValueType = AttributeType.Int;
-            s1.Condition.LeftCondition.AtomCondition.RightOperand.Value = 2;
-
-            //右条件
-            s1.Condition.RightCondition = new Condition();
-            s1.Condition.RightCondition.AtomCondition = new AtomCondition();
-            s1.Condition.RightCondition.AtomCondition.Operator = LogicOperator.Equal;
-
-            s1.Condition.RightCondition.AtomCondition.LeftOperand.IsValue = false;
-            s1.Condition.RightCondition.AtomCondition.LeftOperand.Field.TableName = "Course";
-            s1.Condition.RightCondition.AtomCondition.LeftOperand.Field.AttributeName = "location";
-
-            s1.Condition.RightCondition.AtomCondition.RightOperand.IsValue = true;
-            s1.Condition.RightCondition.AtomCondition.RightOperand.ValueType = AttributeType.String;
-            s1.Condition.RightCondition.AtomCondition.RightOperand.Value = "CB‐6";
-
-            System.Console.WriteLine("\n\nSample1 SQL:");
-            System.Console.WriteLine(s1.ToString());
-
-
-            //Sample2
-            /*
-             * select Course.name, Course.credit_hour, Teacher.name
-             * from Course, Teacher
-             * where Course.teacher_id=Teacher.id and
-             * Course.credit_hour>2 and
-             * Teacher.title=3
-             */
-
-            s2 = new Selection();
-            
-            //Select
-            s2.Fields.IsAllFields = false;
-            s2.Fields.TableName = "";
-            Field f1 = new Field(); 
-            f1.TableName = "Course";
-            f1.AttributeName = "name";
-            s2.Fields.Fields.Add(f1); //Course.name
-
-            f1 = new Field();
-            f1.TableName = "Course";
-            f1.AttributeName = "credit_hour";
-            s2.Fields.Fields.Add(f1); //Course.credit_hour
-
-            f1 = new Field();
-            f1.TableName = "Teacher";
-            f1.AttributeName = "name";
-            s2.Fields.Fields.Add(f1); //Teacher.name
-
-
-            //From Course
-            table = new TableScheme();
-            table.TableName = "Course";
-            table.IsAllFields = true;
-            s2.Sources.Add(table);
-
-            //From Teacher
-            table = new TableScheme();
-            table.TableName = "Teacher";
-            table.IsAllFields = true;
-            s2.Sources.Add(table);
-            s2.Condition = new Condition();
-            s2.Condition.Operator = RelationOperator.And;
-            //左条件 Course.teacher_id=Teacher.id
-            s2.Condition.LeftCondition = new Condition();
-            s2.Condition.LeftCondition.Content = "Course.teacher_id=Teacher.id ";
-            s2.Condition.LeftCondition.AtomCondition = new AtomCondition();
-            s2.Condition.LeftCondition.AtomCondition.Operator = LogicOperator.Equal;
-
-            s2.Condition.LeftCondition.AtomCondition.LeftOperand.IsValue = false;
-            s2.Condition.LeftCondition.AtomCondition.LeftOperand.Field.AttributeName = "teacher_id";
-            s2.Condition.LeftCondition.AtomCondition.LeftOperand.Field.TableName = "Course";
-
-            s2.Condition.LeftCondition.AtomCondition.RightOperand.IsValue = false;
-            s2.Condition.LeftCondition.AtomCondition.RightOperand.Field.AttributeName = "id";
-            s2.Condition.LeftCondition.AtomCondition.RightOperand.Field.TableName = "Teacher";
-
-            //右条件 Course.credit_hour>2 and  Teacher.title=3
-            s2.Condition.RightCondition = new Condition();
-            s2.Condition.RightCondition.Content = "Course.credit_hour>2 and  Teacher.title=3";
-            s2.Condition.RightCondition.Operator = RelationOperator.And;
-
-            //右条件的左条件 Course.credit_hour>2
-            s2.Condition.RightCondition.LeftCondition = new Condition();
-            s2.Condition.RightCondition.LeftCondition.AtomCondition = new AtomCondition();
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.Operator = LogicOperator.Greater; //  ">"
-            
-            //Course.credit_hour
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.LeftOperand.IsValue = false;
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.LeftOperand.Field.TableName = "Course";
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.LeftOperand.Field.AttributeName = "credit_hour";
-
-            //2
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.RightOperand.IsValue = true;
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.RightOperand.ValueType = AttributeType.Int;
-            s2.Condition.RightCondition.LeftCondition.AtomCondition.RightOperand.Value = 2;
-
-            //右条件的右条件 Teacher.title=3
-            s2.Condition.RightCondition.RightCondition = new Condition();
-            s2.Condition.RightCondition.RightCondition.AtomCondition = new AtomCondition();
-            s2.Condition.RightCondition.RightCondition.AtomCondition.Operator = LogicOperator.Equal; //  "="
-
-            // Teacher.title
-            s2.Condition.RightCondition.RightCondition.AtomCondition.LeftOperand.IsValue = false;
-            s2.Condition.RightCondition.RightCondition.AtomCondition.LeftOperand.Field.TableName = "Teacher";
-            s2.Condition.RightCondition.RightCondition.AtomCondition.LeftOperand.Field.AttributeName = "title";
-
-            //3
-            s2.Condition.RightCondition.RightCondition.AtomCondition.RightOperand.IsValue = true;
-            s2.Condition.RightCondition.RightCondition.AtomCondition.RightOperand.ValueType = AttributeType.Int;
-            s2.Condition.RightCondition.RightCondition.AtomCondition.RightOperand.Value = 3;
-
-            System.Console.WriteLine("\nSample2 SQL:");
-            System.Console.WriteLine(s2.ToString());
-
             //测试
             string[] tests = new string[]{
            "select * from Student",
@@ -295,7 +162,11 @@ namespace DistDBMS.ControlSite
                 bool result = ps.Parse(tests[i]);
                 Selection s3 = ps.LastResult as Selection;
                 GlobalConsitencyFiller filler = new GlobalConsitencyFiller();
+                //这一步很重要，通过gdd来填写selection的完整信息
                 filler.FillSelection(gdd, s3);
+
+                //TO 刘璋：可以从这里开始测试转换
+
 
 
 
@@ -304,55 +175,6 @@ namespace DistDBMS.ControlSite
                 System.Console.WriteLine("Parse:" + s3.ToString());
             }
 
-            string allocate = "allocate Student.2 to S2";
-            bool r = ps.Parse(allocate);
-            if (r)
-            {
-                System.Console.Write("\n\nALLOCATE：" + (ps.LastResult as Allocation).ToString());
-            }
-
-            string insert = "insert into Student values (190001, 'xiao ming', 'M', 20, 1)";
-            r = ps.Parse(insert);
-            if (r)
-            {
-                System.Console.Write("\n\nInsert：" + (ps.LastResult as Insertion).ToString());
-            }
-
-            string delete = "delete from Teacher where id>=290000 and title=2";
-            r = ps.Parse(delete);
-            if (r)
-            {
-                System.Console.Write("\n\nDelete：" + (ps.LastResult as Deletion).ToString());
-            }
-
-            string define = "define site S3 127.0.0.1:2003";
-            r = ps.Parse(define);
-            if (r)
-            {
-                System.Console.Write("\n\nSite Define：" + (ps.LastResult as SiteDefinition).ToString());
-            }
-
-            string create = "create table Student (id int key, name char(25), sex char(1), age int, degree int)";
-            r = ps.Parse(create);
-            if (r)
-            {
-                System.Console.Write("\n\nTable Create：" + (ps.LastResult as TableCreation).ToString());
-            }
-
-            string hfrag = "fragment Teacher horizontally into id<201000 and title<>3, id<201000 and title=3, id>=201000 and title<>3, id>=201000 and title=3";
-            r = ps.Parse(hfrag);
-            if (r)
-            {
-                System.Console.Write("\n\nfragment：" + (ps.LastResult as HFragmentation).ToString());
-            }
-
-            
-            string vfrag = "fragment Course vertically into (id, name), (id, location, credit_hour, teacher_id)";
-            r = ps.Parse(vfrag);
-            if (r)
-            {
-                System.Console.Write("\n\nfragment：" + (ps.LastResult as VFragmentation).ToString());
-            }
 
         }
 
@@ -374,20 +196,20 @@ namespace DistDBMS.ControlSite
                 sr.Close();
                 gdd = gddCreator.CreateGDD();
 
-                FragmentFinder finder = new FragmentFinder(gdd);
-                Condition condition = new Condition();
-                condition.AtomCondition = new AtomCondition();
-                condition.AtomCondition.LeftOperand.Field.TableName = "Course";
-                condition.AtomCondition.LeftOperand.Field.AttributeName = "credit_hour";
-                condition.AtomCondition.LeftOperand.IsField = true;
+                //FragmentFinder finder = new FragmentFinder(gdd);
+                //Condition condition = new Condition();
+                //condition.AtomCondition = new AtomCondition();
+                //condition.AtomCondition.LeftOperand.Field.TableName = "Course";
+                //condition.AtomCondition.LeftOperand.Field.AttributeName = "credit_hour";
+                //condition.AtomCondition.LeftOperand.IsField = true;
 
-                condition.AtomCondition.Operator = LogicOperator.Greater;
-                condition.AtomCondition.RightOperand.IsValue = true;
-                condition.AtomCondition.RightOperand.ValueType = AttributeType.Int;
-                condition.AtomCondition.RightOperand.Value = 2;
+                //condition.AtomCondition.Operator = LogicOperator.Greater;
+                //condition.AtomCondition.RightOperand.IsValue = true;
+                //condition.AtomCondition.RightOperand.ValueType = AttributeType.Int;
+                //condition.AtomCondition.RightOperand.Value = 2;
 
-
-                finder.GetMatchFragments(condition);
+                //这个类还没完成，可能需要刘璋完成，想用来匹配条件，也可以不用
+                //finder.GetMatchFragments(condition);
 
 
             }
