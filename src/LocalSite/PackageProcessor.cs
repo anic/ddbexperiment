fa76da9749f51955f102a9426a4cb480dd5a2df1
yaoancheng <yaoancheng@1f297eaa-a758-11dd-9a14-c1645b63b0c6>
2008-12-11 02:50:51 +0000

    网络连调通了！！！

--- src/LocalSite/PackageProcessor.cs
+++ src/LocalSite/PackageProcessor.cs
@@ -15,13 +15,18 @@ namespace DistDBMS.LocalSite
         string name;
         public string Name { get { return name; } }
         VirtualBuffer buffer;
+        ExecutionPlan currentPlan;
+        LocalDirectory ldd;
         public PackageProcessor(string localSiteName)
         {
             this.name = localSiteName;
             this.buffer = new VirtualBuffer();
+
+            //等待数据来才初始化
+            this.currentPlan = null;
+            this.ldd = null;
         }
 
-        LocalDirectory ldd;
 
         public void LocalSitePackageProcess(LocalSiteServerConnection conn, LocalSiteServerPacket packet)
         {
@@ -50,64 +55,19 @@ namespace DistDBMS.LocalSite
                         if (result)
                             conn.SendServerClientTextPacket(Common.NetworkCommand.RESULT_OK);
                         else
-                            conn.SendServerClientTextPacket("BAD");
+                            conn.SendServerClientTextPacket(Common.NetworkCommand.RESULT_ERROR);
                     }
                     else if (package.Type == ExecutionPackage.PackageType.Plan)//执行计划
                     {
                         ExecutionPlan plan = package.Object as ExecutionPlan;
-                        QueryProcessor processor = new QueryProcessor(ldd);
-                        foreach (ExecutionStep step in plan.Steps)
+                        if (currentPlan == null)
                         {
-                            if (step.Type == ExecutionStep.ExecuteType.Select)
-                            {
-                                while (true)
-                                {
-                                    bool allReady = true;
-                                    lock (buffer)
-                                    {
-                                        for (int i = 0; i < step.WaitingId.Count; i++)
-                                            allReady &= (buffer.GetPackageById(step.WaitingId[i]) != null);
-
-                                        if (allReady)
-                                            break;
-                                    }
-
-                                    Thread.Sleep(new Random().Next(1000)); //现在是停等，以后应该是异步等，或者唤醒机制
-                                }
-
-                                lock (buffer)
-                                {
-                                    Table table = processor.Handle(step, name, buffer);
-                                    //TODO:做异常处理
-                                    //if(table==null)
-
-                                    ExecutionPackage newPackage = new ExecutionPackage();
-                                    newPackage.ID = step.Operation.ResultID;
-                                    newPackage.Type = ExecutionPackage.PackageType.Data;
-                                    newPackage.Object = table;
-                                    buffer.Add(newPackage);//相当于异步发送
-                                    if (step.TransferSite != null)
-                                        conn.SendP2PStepTextObjectPacket(step.TransferSite.Name,
-                                            Network.SessionStepPacket.StepIndexNone,
-                                            Common.NetworkCommand.EXESQL,
-                                            newPackage);
-                                    else if (step.Index == 0) //返回ControlSite
-                                        conn.SendServerClientTextObjectPacket(Common.NetworkCommand.RESULT_OK, newPackage);
-                                    else
-                                        buffer.Add(newPackage);
-                                }
-                            }
-                            else if (step.Type == ExecutionStep.ExecuteType.Insert)
-                            {
-                                using (DataAccess.DataAccessor da = new DistDBMS.LocalSite.DataAccess.DataAccessor(name))
-                                {
-                                    TableSchema logicSchema = ldd.Fragments.GetFragmentByName(step.Table.Name).LogicSchema;
-                                    step.Table.Schema.ReplaceTableName(logicSchema.TableName);
-                                    int result = da.InsertValues(step.Table);
-                                }
-                            }
+                            currentPlan = plan;
+                            //能执行多少执行多少
+                            ExecutePlan(conn);
                         }
-                        conn.SendServerClientTextPacket(Common.NetworkCommand.RESULT_OK);
+                        else
+                            conn.SendServerClientTextObjectPacket(Common.NetworkCommand.RESULT_ERROR, "BUSY");
                     }
                 }
             }
@@ -121,10 +81,89 @@ namespace DistDBMS.LocalSite
                 if ((packet as P2PTextObjectPacket).Object is ExecutionPackage)
                 {
                     lock (buffer)
-                    {
                         buffer.Add((packet as P2PTextObjectPacket).Object as ExecutionPackage);
+
+                    ExecutePlan(conn);
+                }
+            }
+        }
+
+        private void ExecutePlan(LocalSiteServerConnection conn)
+        {
+            ExecutionPlan plan = currentPlan;
+            if (plan == null)
+                return;
+
+            QueryProcessor processor = new QueryProcessor(ldd);
+            bool allDone = true;
+            foreach (ExecutionStep step in plan.Steps)
+            {
+                if (step.Type == ExecutionStep.ExecuteType.Select)
+                {
+                    if (step.Status!=null && step.Status.Done) //步骤已经完成
+                        continue;
+
+                    bool allWaitedPackageArrived = true; //所有等待的包都到齐了
+                    lock (buffer)
+                    {
+                        //TODO:现在在我的Buffer找，以后在Packect中找
+                        for (int i = 0; i < step.WaitingId.Count; i++)
+                            allWaitedPackageArrived &= (buffer.GetPackageById(step.WaitingId[i]) != null);
+
+                        //如果数据到齐了，执行，否则执行下一个步骤
+                        if (allWaitedPackageArrived)
+                        {
+                            step.Status = new Status();
+                            step.Status.StartTime = DateTime.Now;
+
+                            Table table = processor.Handle(step, name, buffer);
+                            //TODO:做异常处理
+                            //if(table==null)
+
+                            step.Status.EndTime = DateTime.Now;
+                            step.Status.Done = true;
+
+                            //产生一个新的Data的包
+                            ExecutionPackage newPackage = new ExecutionPackage();
+                            newPackage.ID = step.Operation.ResultID;
+                            newPackage.Type = ExecutionPackage.PackageType.Data;
+                            newPackage.Object = table;
+
+                            buffer.Add(newPackage);//相当于异步发送
+                            if (step.TransferSite != null && step.TransferSite.Name != this.name)
+                            {
+                                conn.SendP2PStepTextObjectPacket(step.TransferSite.Name,
+                                    newPackage.ID,
+                                    Common.NetworkCommand.EXESQL,
+                                    newPackage);                                
+                            }
+                            else if (step.Index == 0) //返回ControlSite
+                                conn.SendServerClientTextObjectPacket(Common.NetworkCommand.RESULT_OK, newPackage);
+                            else
+                                buffer.Add(newPackage);
+
+                        }
+                        else
+                            allDone = false;
                     }
                 }
+                else if (step.Type == ExecutionStep.ExecuteType.Insert)
+                {
+                    using (DataAccess.DataAccessor da = new DistDBMS.LocalSite.DataAccess.DataAccessor(name))
+                    {
+                        TableSchema logicSchema = ldd.Fragments.GetFragmentByName(step.Table.Name).LogicSchema;
+                        step.Table.Schema.ReplaceTableName(logicSchema.TableName);
+                        int result = da.InsertValues(step.Table);
+                    }
+                }
+            }
+
+            if (allDone)
+            {
+                conn.SendServerClientTextPacket(Common.NetworkCommand.RESULT_OK);
+                currentPlan = null;
+                lock (buffer)
+                    buffer.Clear();
             }
         }
 
